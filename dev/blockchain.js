9ac64247de4cc870385a6017c5a62d453e89b5ae
masakistewart <stewartcairo@gmail.com>
2021-07-14 11:09:05 -0700

    add export oldstyle

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,22 +1,24 @@
 class Blockchain {
-    constructor() {
-        this.chain = [];
-        this.newTransactions = [];
-    }
+  constructor() {
+    this.chain = [];
+    this.newTransactions = [];
+  }
 
-    createNewBlock = (nonce, previousBlockHash, hash) => {
-        const newBlock = {
-            index: this.chain.length + 1,
-            timestamp: Date.now(),
-            transactions: this.newTransactions,
-            nonce: nonce,
-            hash: hash,
-            previousBlockHash: previousBlockHash,
-        };
+  createNewBlock(nonce, previousBlockHash, hash) {
+    const newBlock = {
+      index: this.chain.length + 1,
+      timestamp: Date.now(),
+      transactions: this.newTransactions,
+      nonce,
+      hash,
+      previousBlockHash,
+    };
 
-        this.newTransactions = [];
-        this.chain.push(newBlock)
+    this.newTransactions = [];
+    this.chain.push(newBlock);
 
-        return newBlock;
-    }
+    return newBlock;
+  }
 }
+
+module.exports = Blockchain;
