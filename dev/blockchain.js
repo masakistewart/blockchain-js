5200562e220230c5b1a6dd1a6125f91748666203
masakistewart <stewartcairo@gmail.com>
2021-07-14 14:16:32 -0700

    add #proofOfWork

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -45,6 +45,21 @@ class Blockchain {
     const hash = sha256(DataAsString);
     return hash;
   }
+
+  proofOfWork(previousBlockHash, currentBlockData) {
+    // repeatedly hash block until it finds correct hash starting with 4 zeros => 0000SOMETHING
+    // uses current block data + previous block hash for the hash
+    // continously changes nonce value until it finds correct the hash,
+    // return the nonce value the matches the correct hash
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    while (hash.substr(0, 4) !== '0000') {
+      nonce += 1;
+      hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    }
+
+    return nonce;
+  }
 }
 
 module.exports = Blockchain;
