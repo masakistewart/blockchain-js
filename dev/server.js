47802f5446e296944b86078c226ea057cad48ed1
masakistewart <stewartcairo@gmail.com>
2021-07-16 07:57:40 -0700

    [WIP] '/mine' endpoint

--- dev/server.js
+++ dev/server.js
@@ -16,13 +16,28 @@ app.get('/blockchain', (req, res) => {
   res.send(coin);
 });
 
-// app.post('/transactions', (req, res) => {
-//   // res.send()
-// });
+app.post('/transaction', (req, res) => {
+  const { amount, sender, recipient } = req.body;
+  const blockIndex = coin.createNewTransaction(amount, sender, recipient);
+  res.json({ note: `Transaction will be added in block ${blockIndex}.` });
+});
 
-// app.get('/mine', (req, res) => {
-//   //
-// });
+app.get('/mine', (req, res) => {
+  // abstract into own fuction
+  const lastBlock = coin.getLastBlock();
+  const previousBlockHash = lastBlock.hash;
+  const currentBlockData = {
+    transactions: coin.pendingTransactions,
+    index: lastBlock.index + 1,
+  };
+  const nonce = coin.proofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = coin.hashBlock(previousBlockHash, currentBlockData, nonce);
+  const newBlock = coin.createNewBlock(nonce, previousBlockHash, blockHash);
+  res.json({
+    note: 'New block mined successfully',
+    block: newBlock,
+  });
+});
 
 app.listen(port, () => {
   console.log(`listening on Port: ${port}`);
