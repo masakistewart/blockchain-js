e5a1ea50c6f4d371f7edcb6961d3cfad81fff001
masakistewart <stewartcairo@gmail.com>
2021-07-16 07:26:10 -0700

    add '/blockchain' response

--- dev/server.js
+++ dev/server.js
@@ -1,23 +1,28 @@
 const express = require('express');
+const Blockchain = require('./blockchain');
 
 const app = express();
 const port = process.env.PORT || 8000;
+const coin = new Blockchain();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('test');
 });
 
 app.get('/blockchain', (req, res) => {
-  res.send('placeholder for /blockchain');
+  res.send(coin);
 });
 
-app.post('/transactions', (req, res) => {
-  // res.send()
-});
+// app.post('/transactions', (req, res) => {
+//   // res.send()
+// });
 
-app.get('/mine', (req, res) => {
-  //
-});
+// app.get('/mine', (req, res) => {
+//   //
+// });
 
 app.listen(port, () => {
   console.log(`listening on Port: ${port}`);
